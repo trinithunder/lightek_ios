26cd88f4c3fbca4d4d6304074ab9a43536193471
Marlon Henry <haveyec@me.com>
2024-03-17 15:07:09 -0400

    First commit

---
+++ Lightek/Lightek/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Lightek
+//
+//  Created by Marlon Henry on 3/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Lightek/Lightek/LightekApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightekApp.swift
+//  Lightek
+//
+//  Created by Marlon Henry on 3/17/24.
+//
+
+import SwiftUI
+
+@main
+struct LightekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
